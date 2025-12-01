bf3ba81ce3598a804e66515c5b73518d36a4ab0f
Nikita Tabulin <mustang1604@gmail.com>
2025-12-01 20:56:58 +0300

    Добавлено спецзадание

--- src/special/SpecialTask.java
+++ src/special/SpecialTask.java
@@ -5,29 +5,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SpecialTask {
-    public static void main(String[] args) {
+    public static void main() {
         Scanner scanner = new Scanner(System.in);
-        String[] paramArray = new String[5];
-
+        String[] args = new String[5];
         System.out.println("Введите 5 параметров :");
         for (int i = 0; i < 5; i++) {
             System.out.print("Параметр " + (i + 1) + ": ");
-            paramArray[i] = scanner.nextLine();
+            args[i] = scanner.nextLine();
         }
 
-        List<String> paramList = Arrays.asList(paramArray);
+        List<String> paramList = Arrays.asList(args);
         scanner.close();
 
-
         Terminal terminal = new Terminal(paramList);
-
 
         String fileName = "C:\\Users\\User\\Desktop\\ОБУЧЕНИЕ\\terminal_data.txt.txt";
 
         try {
-            FileWriter writer = new FileWriter(fileName);
-            writer.write(terminal.toString());
-            writer.close();
+           // FileWriter writer = new FileWriter(fileName);
+           // writer.write(terminal.toString());
+           // writer.close();
+            FileOutputStream fileOutputStream=new FileOutputStream(fileName);
+            ObjectOutputStream objectOutputStream= new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(terminal.parameters);
             System.out.println("Объект Terminal сохранен в текстовый файл '" + fileName + "'.");
 
         } catch (IOException e) {
--- src/special/Terminal.java
+++ src/special/Terminal.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 
 public class Terminal implements Serializable {
-    private List<String> parameters;
+     List<String> parameters;
 
     public Terminal(List<String> parameters) {
         this.parameters = parameters;
